419ad3dfd055061e0717231549dcebb886cf46ef
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-06-28 02:09:54 +0530

    formated user model

--- model/user.js
+++ model/user.js
@@ -1,21 +1,16 @@
-const crypto = require('crypto');
-const sql = require('../config/db');
+const crypto = require("crypto");
+const sql = require("../config/db");
 
-
-// function to create secured hashed passeord 
-let createSecurePassword = (password , salt ) =>{
-    if(!password) return "";
-    try{
-    return crypto.createHmac('sha256',salt).
-                  update(password).
-                  digest("hex");
-    }catch(error){
-        return "";
-    }
-}
-
-
+// function to create secured hashed passeord
+let createSecurePassword = (password, salt) => {
+  if (!password) return "";
+  try {
+    return crypto.createHmac("sha256", salt).update(password).digest("hex");
+  } catch (error) {
+    return "";
+  }
+};
 
 module.exports = {
-    createSecurePassword : createSecurePassword
+  createSecurePassword: createSecurePassword,
 };
