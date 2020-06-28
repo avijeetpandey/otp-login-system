d3d92874a26bbf8db3e7637d66782a318379fc37
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-06-28 17:49:18 +0530

    added server log support

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const logger=require('morgan')
+const path=require('path')
+const fs=require('fs')
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,10 +13,18 @@ const registerRoute = require("./routes/register");
 const loginRoute = require("./routes/login");
 const validateOTP =require('./routes/otp');
 
+//writting stream for logs
+const logStream=fs.createWriteStream(path.join(__dirname,'server.log'),{flags:'a'});
+
+
 // middlewares
+app.use(logger('dev'));
+app.use(logger('combined', { stream: logStream }));
+
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set('view engine','ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/',(req,res)=>{
     res.render('register');
@@ -20,6 +32,9 @@ app.get('/',(req,res)=>{
 app.use("/register", registerRoute);
 app.use("/login", loginRoute);
 app.use('/otp',validateOTP);
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on PORT : ${PORT}`);
 });
